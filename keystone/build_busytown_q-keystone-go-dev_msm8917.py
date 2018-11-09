52dae98303217e73c687bf53e8862a39f4374df2
Tim Joines <tjoines@google.com>
2018-11-09 14:59:17 -0800

    Use build_busytown.DEFAULT_BUILD_GOALS +
    make platform_tests dist and droid all work in our tree, so compile
    them on the build server.
    
    Bug: 118442372
    Test: manual
    Change-Id: I08238881252417f35cf075dcb787ff6acc839dd4

--- keystone/build_busytown_q-keystone-go-dev_msm8917.py
+++ keystone/build_busytown_q-keystone-go-dev_msm8917.py
@@ -4,7 +4,7 @@
 import build_busytown
 
 # TODO(b/119317944): Remove SKIP_ABI_CHECKS
-build_goals = [
+build_goals = build_busytown.DEFAULT_BUILD_GOALS + [
     'TEMPORARY_DISABLE_PATH_RESTRICTIONS=true', 'SKIP_ABI_CHECKS=true', '-k'
 ]
 
