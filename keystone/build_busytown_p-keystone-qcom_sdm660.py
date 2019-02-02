5e438899fd814705f638ac4a6702f79c71444715
Steven Laver <lavers@google.com>
2019-02-01 16:12:49 -0800

    Remove keystone and tools/metamon from platform/development repo
    Bug: 123652761
    Change-Id: I241b2f569587eab3dffc4722f9b109f8299695bb

--- keystone/build_busytown_p-keystone-qcom_sdm660.py
+++
@@ -1,6 +0,0 @@
-"""Builds p-keystone-qcom sdm660 on an Busytown build host.
-"""
-
-import build_busytown
-
-build_busytown.build_target('sdm660_64', 'userdebug')
--- keystone/build_busytown_q-keystone-qcom-dev_cuttlestone_x86_phone.py
+++
@@ -1,7 +0,0 @@
-"""Builds q-keystone-qcom-dev cuttlestone_x86_phone on a Busytown build host.
-"""
-
-import build_busytown
-
-build_goals = build_busytown.DEFAULT_BUILD_GOALS + ['tests']
-build_busytown.build_target('cuttlestone_x86_phone', 'userdebug', build_goals)
